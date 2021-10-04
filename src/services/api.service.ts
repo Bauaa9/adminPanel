d13570158567b32b74710ac64bc5d694aceb92c4
Rohit <ghodkerohit999@gmail.com>
2021-10-04 15:27:50 +0530

    saved token to session storege

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -9,12 +9,12 @@ export class ApiService {
   constructor(private http: HttpClient) { }
 
   public api(methodName:string,body:any,url:string, headerWithToken?: boolean){
-    let token = localStorage.getItem('userToken');
+    let token = sessionStorage.getItem('userToken');
     let headers={
       'Content-Type':'application/json'
     };
+    console.log(token)
     if(headerWithToken==true){
-      token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJyb2hpdCIsImV4cCI6MTYzMzM0MDMzMSwiaWF0IjoxNjMzMzIyMzMxfQ.Ovi2lDFPwJzgX9zHtmZpeJFesct4mDm0eRhVmP9uUKylPxmtszqCa9E-vC0ppYxd7wqqUe1YagSIJKh28Sug0Q';
       headers = {
         'Content-Type':'application/json',
         // @ts-ignore
